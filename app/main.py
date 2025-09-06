826f598f4e7a6363038157003337f15e773f6cdc
mestre <pedromestre2006@gmail.com>
2025-09-06 09:12:30 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -14,6 +15,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file" and sys.argv[1] == "-p":
+        file = sys.argv[3]
+        obj_path = f".git/objects/{file[:2]/file[2:]}"
+        with open(obj_path, "rb") as obj:
+            content = obj.read
+            content = zlib.decompress(content)
+            content_start = content.find(b"x00")
+            content = content[content_start + 1:].strip()
+            print(content.decode("utf-8"), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
